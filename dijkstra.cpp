7c5675147408b766675f161458d81f79b7942c9e
diiibe <48642236+diiibe@users.noreply.github.com>
2023-02-04 18:21:07 +0100

    corretto e quasi funzionante

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,29 +1,43 @@
-
 template <typename T>
-std::unordered_map<T, int> Graph<T>::dijkstra(Graph<T>& graph, T src) {
+std::unordered_map<T, int> dijkstra(Graph<T> g, T src)
+{
     // implementazione dell'algoritmo di Dijkstra
-            std::unordered_map<T, int> dist;
-            std::unordered_map<T, T> prev;
-            std::unordered_map<T, int> INT_MAX;
-    
-            // Inizializzazione
-            for (auto it = g.v_begin(); it != g.v_end(); ++it) {
-                dist[it] = INT_MAX;
-                prev[it] = -1;
-            }
-            dist[src] = 0;
+    std::unordered_map<T, int> dist;        //mappa di distanze A : 6, C : 9
+    std::unordered_map<T, T> prev;            //precedente: C:A -> C precede A, per estrarre il PRECEDENTE si chiama FIRST
+    std::vector<T> queue;                    //coda di vertici
 
-            // Relaxazione degli archi |V|-1 volte
-            for (int i = 0; i < vertices.size() - 1; i++)            
-                for (auto u = g.v_begin(); it != g.v_end(); ++it) {
-                    for (auto e = g.e_begin(); it != g.e_end(); ++it)) {
-                        T v = e.first;
-                        int weight = e.second;
-                        if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) {
-                            dist[v] = dist[u] + weight;
-                            prev[v] = u;
-                        }
+    // Inizializzazione
+    for (auto it = g.v_begin(); it != g.v_end(); ++it)        //itero i vertici con vertex_iterator
+    {
+        dist[it] = INT_MAX;                                //la distanza da src va a infinito
+        prev[it] = -1;                                        //il precedente è -1, = null
+        queue.push_back(it);                                //aggiungo il vertice alla coda
+    }
+    dist.at(src) = 0;    //la distanza da src a src è 0 ovviamente
+
+    //iterate through all vertices
+        for (int i = 0; i < g.getVertices().size() - 1; i++)
+        {
+            for (auto it = g.v_begin(); it != g.v_end(); ++it)        //vrtx corrente =it
+            {
+                auto nears = g.getEdges().at(it);    //nears contiene i vicini del vertice corrente
+
+                //per ogniuno dei vicini,
+                //se la distanza è 
+
+                for (auto e_it = nears.begin(); e_it != nears.end(); ++it)
+                {
+                    auto v = e_it->first;
+                    int weight = e_it->second;
+                    if (dist.at(it) != INT_MAX && dist.at(v) + weight < dist.at(v))
+                    {
+                        dist.at(v) = dist.at(it) + weight;
+                        prev.at(v) =it;
                     }
+
                 }
             }
-        };
+        }
+
+    return dist;
+}
