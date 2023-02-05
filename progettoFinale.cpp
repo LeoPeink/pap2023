c1e231447194e84d1403102ef376415dc3a809a2
LeoPeink <79313897+LeoPeink@users.noreply.github.com>
2023-02-05 19:15:50 +0100

    djikstra e bf funzionano perfettamente.

--- progettoFinale.cpp
+++ progettoFinale.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
-#include "errorMessages.h"
 #include <algorithm>
+#include "errorMessages.h"
 
 //todo dividere in files
 
@@ -45,8 +45,6 @@ public:
 		inline bool operator!=(const vertex_iterator& it) { return ptr !=it.ptr; }			//operatore disuguaglianza
 		//NB:inline per efficienza, non serve chiamare la funzione, basta far copiare il codice al compilatore al momento della chiamata
 		
-
-
 		//iterator(typename std::vector<T>::iterator it) : it{ it } {}; 
 
 	private:
@@ -57,8 +55,8 @@ public:
 		friend class Graph<T>;	//permette a Graph di accedere ai membri privati di iterator
 	};
 
-	inline vertex_iterator v_begin() { return vertex_iterator{ &vertices[0] }; }	//ritorna un iteratore al primo elemento del vettore, TODO CHECK SE FUNZIONA
-	inline vertex_iterator v_end() { return vertex_iterator{ &vertices[vertices.size() - 1] + 1 }; }	//ritorna un iteratore all'ultimo elemento del vettore,	TODO CHECK SE FUNZIONA
+	inline vertex_iterator v_begin() { return vertex_iterator{ &vertices[0] }; }	//ritorna un iteratore al primo elemento del vettore,
+	inline vertex_iterator v_end() { return vertex_iterator{ &vertices[vertices.size() - 1] + 1 }; }	//ritorna un iteratore all'ultimo elemento del vettore,
 
 	class edge_iterator 
 	{
@@ -157,33 +155,112 @@ void Graph<T>::removeEdge(T src, T dest) {
 	}
 }
 
-template <typename T>
-void djikstra(Graph<T> g, std::string start)
-{
-	/*
-	for (T& val : g.getVertices())		//ITERATORE PER I VERTICI: FUNZIONA!!!!
-	{
-		std::cout << val << std::endl;
-	}
-	*/
-	//std::unordered_map<T, int> dist;
-	// .at() per gettare l'elemento dalla key, ma qua non serve (iteratore?)
-	/*
-	for each vertex V in G
-		dist[V] < -infinite
-		prev[V] < -NULL
-		If V != S, add V to Priority Queue Q
-		dist[S] < -0
 
-		while Q IS NOT EMPTY
-			U < -Extract MIN from Q
-			for each unvisited neighbour V of U
-				temperoryDist < -dist[U] + edgeWeight(U, V)
-				if temperoryDist < dist[V]
-					dist[V] < -temperoryDist
-					prev[V] < -U
-					return dist[], prev[]
-					*/
+template <typename T>
+std::unordered_map<T, int> dijkstra(Graph<T> g, T src)		//TODO min heap per la coda, ora è v2
+{
+	// implementazione dell'algoritmo di Dijkstra
+	std::unordered_map<T, int> dist;		//mappa di distanze A : 6, C : 9
+	std::unordered_map<T, T> prev;			//precedente: C:A -> C precede A, per estrarre il PRECEDENTE si chiama FIRST
+	//std::vector<T> queue;					//coda di vertici
+
+	// Inizializzazione
+	for (auto it = g.v_begin(); it != g.v_end(); ++it)		//itero i vertici con vertex_iterator
+	{
+		dist[*it] = INT_MAX;								//la distanza da src va a infinito
+		prev[*it] = -1;										//il precedente è -1, = null
+		//queue.push_back(*it);								//aggiungo il vertice alla coda
+	}
+	dist.at(src) = 0;	//la distanza da src a src è 0 ovviamente
+
+	//iterate through all vertices
+	for (int i = 0; i < g.getVertices().size() - 1; i++) //TODO coda di priorità
+	{
+		for (auto it = g.v_begin(); it != g.v_end(); ++it)		//vrtx corrente = *it
+		{
+
+			//std::cout << "VERTICE IN ANALISI: " << *it << std::endl;
+
+			try
+			{
+				auto nears = g.getEdges().at(*it);	//nears contiene i vicini del vertice corrente
+				for (auto e_it = nears.begin(); e_it != nears.end(); ++e_it) //per ogniuno dei vicini (gli adiacenti al vrtx corrente)
+				{
+					auto v = e_it->first;									//"v" contiene il nome del vicino, es. "A"
+					int weight = e_it->second;								//weight contiene il peso dell'arco, es. 5. Significato: da *it a v c'è un arco pesante weight.
+
+					//std::cout << "ARCO:" << *it << " -> " << v << ", PESO: " << weight << std::endl;
+
+					if (dist.at(*it) != INT_MAX && (dist.at(*it) + weight) < dist.at(v)) //se la distanza non è INF e  
+					{
+						dist.at(v) = dist.at(*it) + weight;
+						prev.at(v) = *it;
+					}
+				}
+				//std::cout << std::endl;
+			}
+			catch(const std::out_of_range e)
+			{
+				//
+			}
+		}
+	}
+
+	return dist;
+}
+
+
+
+	
+template <typename T>
+std::unordered_map<T, int> bellmanFord(Graph<T> g, T src)
+{
+	std::unordered_map<T, int> dist;		//mappa di distanze A : 6, C : 9
+
+	// Step 1: fill the distance array and predecessor array
+	for (auto it = g.v_begin(); it != g.v_end(); ++it)		//itero i vertici con vertex_iterator
+	{
+		dist[*it] = INT_MAX;								//la distanza da src va a infinito
+	}
+
+	// Mark the source vertex
+	dist.at(src) = 0;	//la distanza da src a src è 0 ovviamente
+
+
+	for (int i = 0; i < g.getVertices().size(); ++i)	//for tutti i vertici
+	{
+		for (auto& lista : g.getEdges())				//per tutti gli archi
+		{
+			auto u = lista.first;			//src, T
+			for (auto& arco : lista.second)				//per tutti gli archi (cont'd)
+			{
+				auto v = arco.first;	//dst, T
+				auto w = arco.second;	//peso, int
+				if (dist.at(u) != INT_MAX && dist.at(u) + w < dist.at(v))
+				{
+					dist.at(v) = dist.at(u) + w;
+				}
+			}
+		}
+	}
+
+	//step 3: 
+	for (auto& lista : g.getEdges())		//per tutti gli archi
+	{
+		auto u = lista.first;				//src, T
+		for (auto& arco : lista.second)				//per tutti gli archi (cont'd)
+		{
+			auto v = arco.first;	//dst, T
+			auto w = arco.second;	//peso, int
+			if (dist.at(u) != INT_MAX && dist.at(u) + w < dist.at(v))
+			{
+				std::cout << E_NEGATIVE_WEIGHT_CYCLE << std::endl;
+				return dist;
+			}
+		}
+	}
+
+	return dist;
 }
 
 
@@ -191,6 +268,29 @@ int main()
 {
 	//instance a new graph named "graph"
 	Graph<std::string> g("graph");
+
+	/*
+	GRAPH for BELLMAN-FORD testing
+	//add vertex
+	g.addVertex("S");
+	g.addVertex("A");
+	g.addVertex("B");
+	g.addVertex("C");
+	g.addVertex("D");
+	g.addVertex("E");
+	//add edges
+	
+	g.addEdge("S", "A", 10);
+	g.addEdge("S", "E", 8);
+	g.addEdge("A", "C", 2);
+	g.addEdge("B", "A", 1);
+	g.addEdge("C", "B", -2);
+	g.addEdge("E", "D", 1);
+	g.addEdge("D", "C", -1);
+	g.addEdge("D", "A", -4);
+	*/
+
+	//GRAPH for DIJKSTRA testing
 	//add vertex
 	g.addVertex("A");
 	g.addVertex("B");
@@ -198,18 +298,45 @@ int main()
 	g.addVertex("D");
 	g.addVertex("E");
 	//add edges
-	g.addEdge("A", "B", 1);
-	g.addEdge("A", "C", 1);
-	g.addEdge("B", "D", 1);
-	g.addEdge("C", "D", 1);
-	djikstra(g, "a");
 
+	g.addEdge("A", "B", 4);
+	g.addEdge("A", "C", 2);
+	g.addEdge("B", "C", 3);
+	g.addEdge("B", "D", 2);
+	g.addEdge("B", "E", 3);
+	g.addEdge("C", "B", 1);
+	g.addEdge("C", "D", 4);
+	g.addEdge("C", "E", 5);
+	g.addEdge("E","D",1);
+	
+	
+	
+	const std::string src = "A"; //soluzione al prob del compilatore, non puoi usare puntatori a char e string costanti come template
+	auto res = dijkstra(g, src);
+
+	for (auto it = res.begin(); it != res.end(); ++it)
+	{
+		std::cout << it->first << " " << it->second << std::endl;
+	}
+	
+	
+	/*
+	auto res = bellmanFord(g1, src);
+	for (auto it = res.begin(); it != res.end(); ++it)
+	{
+		std::cout << it->first << " " << it->second << std::endl;
+	}
+	*/
+
+
+	/*
 	//TEST iteratore per i vertici
 	for (auto it = g.v_begin(); it != g.v_end(); ++it)
 	{
 		std::cout << *it << std::endl;
 	}
 
+	//TEST iteratore per gli archi
 	for (auto& lista : g.getEdges())
 	{
 		std::cout << lista.first << std::endl;
@@ -217,16 +344,7 @@ int main()
 		{
 			std::cout << arco.first << " " << arco.second << std::endl;
 		}
-			
-	}
-
-	/*
-	//print all existing edges
-	for (auto it = g.e_begin(); it != g.e_end(); ++it)
-	{
-		it->first;
-		
-		//std::cout << it->first << " " << it->second << std::endl;
 	}
 	*/
+
 }
