55cd83427c28f01027e9251dd8c312fe9ee4cfc9
LeoPeink <79313897+LeoPeink@users.noreply.github.com>
2023-02-07 18:33:40 +0100

    new errors added

--- errorMessages.h
+++ errorMessages.h
@@ -1,6 +1,10 @@
+#pragma once
 
 //file per mantenere in modo ordinato i diversi messaggi di errore che potrebbero presentarsi durante l'uso dell'interfaccia.
 
-const std::string E_VERTEX_ALREADY_EXISTING	{ "ERROR: VERTEX ALREADY EXISTING\n" };
-const std::string E_MISSING_VERTEX			{ "ERROR: ONE OR MORE VERTICES NOT FOUND - UNABLE TO CREATE EDGE\n" };
-const std::string E_NEGATIVE_WEIGHT_CYCLE 			{ "ERROR: A NEGATIVE CYCLE IS PRESENT IN THE GRAPH, UNABLE TO PERFORM COMPUTATION\n" };
+const std::string E_VERTEX_ALREADY_EXISTING	{ "ERROR: SPECIFIED VERTEX ALREADY EXISTING.\n" };
+const std::string E_MISSING_VERTEX			{ "ERROR: SPECIFIED VERTEX NOT FOUND.\n" };
+const std::string E_NEGATIVE_WEIGHT_CYCLE	{ "WARNING: NEGATIVE CYCLE DETECTED. SOLUTION MAY NOT BE OPTIMAL.\n" };
+const std::string E_MISSING_EDGE			{ "ERROR: SPECIFIED EDGE NOT FOUND.\n" };
+const std::string E_EMPTY_GRAPH				{ "ERROR: ALGORITHM CALLED ON EMPTY GRAPH.\n"};
+const std::string E_NEGATIVE_GRAPH			{ "WARNING: NEGATIVE WEIGHT DETECTED. SOLUTION MAY NOT BE OPTIMAL.\n." };
