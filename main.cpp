1a1f61ecb1d6e0ce3054eb3b92e956b40be8f2ee
LeoPeink <79313897+LeoPeink@users.noreply.github.com>
2023-02-07 18:33:06 +0100

    final version with demo interface

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
-//small CLI interface for Graph testing.
-
-
-#include <iostream>
-#include "progettoFinale.cpp"
+#include "graph.CPP"
 
 int main()
 {
-
 	int selection = 0;
 	Graph<std::string> g;
 	do
 	{
-		std::cout << "---GRAPH DEMO:---" << std::endl;
-
-		std::cout << "1) Generate a demo graph as for graph_demo_1.png" << std::endl;
-		std::cout << "2) Generate a demo graph as for graph_demo_2.png" << std::endl;
+		std::cout << std::endl << "---GRAPH DEMO:---" << std::endl << std::endl;
+		std::cout << "0) Generate a demo graph as for graph_demo_0.png. This graph contains a negative cycle." << std::endl;
+		std::cout << "1) Generate a demo graph as for graph_demo_1.png. This graph has only positive edges." << std::endl;
+		std::cout << "2) Generate a demo graph as for graph_demo_2.png.This graph has positive and negative edges." << std::endl;
 		std::cout << "3) Generate an empty graph and add custom Vertices and Edges" << std::endl;
 		std::cout << "4) Execute and print results of Dijkstra Algorithm on the graph" << std::endl;
 		std::cout << "5) Execute and print results of Bellman-Ford Algorithm on the graph" << std::endl;
@@ -22,6 +17,29 @@ int main()
 		std::cin >> selection;
 		switch (selection)
 		{
+		case 0:
+		{
+			//GRAPH for BELLMAN-FORD testing, as for graph_demo_0.png 
+			//add vertices
+			g = Graph<std::string>("demo_graph_2");
+			g.addVertex("S");
+			g.addVertex("A");
+			g.addVertex("B");
+			g.addVertex("C");
+			g.addVertex("D");
+			g.addVertex("E");
+			//add edges
+			g.addEdge("S", "A", 10);
+			g.addEdge("S", "E", 8);
+			g.addEdge("A", "C", -2);
+			g.addEdge("B", "A", 1);
+			g.addEdge("C", "B", -2);
+			g.addEdge("E", "D", 1);
+			g.addEdge("C", "D", -1);
+			g.addEdge("D", "A", -4);
+			std::cout << "graph_demo_0 has been loaded and is ready." << std::endl;
+			break;
+		}		
 		case 1:
 		{
 			//GRAPH for DIJKSTRA testing, as for graph_demo_1.png 
@@ -42,7 +60,7 @@ int main()
 			g.addEdge("C", "D", 4);
 			g.addEdge("C", "E", 5);
 			g.addEdge("E", "D", 1);
-			std::cout << "graph_demo_1 ready." << std::endl;
+			std::cout << "graph_demo_1 has been loaded and is ready." << std::endl;
 			break;
 		}
 		case 2:
@@ -65,10 +83,9 @@ int main()
 			g.addEdge("E", "D", 1);
 			g.addEdge("D", "C", -1);
 			g.addEdge("D", "A", -4);
-			std::cout << "graph_demo_2 ready." << std::endl;
+			std::cout << "graph_demo_2 has been loaded and is ready." << std::endl;
 			break;
 		}
-
 		case 3:
 		{
 			int numVer;
@@ -81,7 +98,8 @@ int main()
 			std::string v = "";
 			for (int i = 0; i < numVer; i++)
 			{
-				std::cout << "Insert vertex n° " << i << std::endl;
+				std::cout << "Lets create vertex n." << i + 1 << ":" << std::endl;
+				std::cout << "Insert vertex name: " << std::endl;
 				std::cin >> v;
 				g.addVertex(v);
 			}
@@ -93,11 +111,12 @@ int main()
 			int w;
 			for (int i = 0; i < numEdges; i++)
 			{
-				std::cout << "Source vertex: " << std::endl;
+				std::cout << "Lets create edge n." << i+1 << ":" << std::endl;
+				std::cout << "Insert source vertex: " << std::endl;
 				std::cin >> s;
-				std::cout << "Destination vertex: " << std::endl;
+				std::cout << "Insert destination vertex: " << std::endl;
 				std::cin >> d;
-				std::cout << "Weigth: " << std::endl;
+				std::cout << "Insert weigth of edge from " << s << " to " << d << std::endl;
 				std::cin >> w;
 				g.addEdge(s, d, w);
 			}
@@ -132,5 +151,6 @@ int main()
 			std::cout << "exiting demo..." << std::endl;
 		}
 
-		}while (selection != 6);
-	}
+		}
+	} while (selection != 6);
+}
